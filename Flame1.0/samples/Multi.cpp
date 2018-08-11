9ad897841e1854863c59f8797df9a53b353162ad
unknown <491691865@qq.com>
2018-08-11 09:37:22 +0800

    Change samples

--- Flame1.0/samples/Multi.cpp
+++ Flame1.0/samples/Multi.cpp
@@ -17,47 +17,60 @@ int main()
 
 	Scene sc;
 
-	//添加边界
+	//添加喷口
+	SDFJet a(0,WATER, BOUNDSDF_P, Vector2d(-2, 0), 0.1);
+	a.Insert(4.7, 1.75);
+	a.Insert(4.8, 1.75);
+	a.Insert(4.8, 1.90);
+	a.Insert(4.7, 1.90);
+	sc.AddObject(&a);
+
+	//添加湿沙子
+	SDFSampleBound b(0, WETSAND, BOUNDSDF_P);
+	b.Insert(2.25, 0.70);
+	b.Insert(3.05, 0.70);
+	b.Insert(3.05, 1.70);
+	b.Insert(2.25, 1.70);
+	sc.AddObject(&b);
+
+	//添加容器边界
 	SDFBoundary c(0, Boundary);
-	c.sdfName = "bond";
-	c.Insert(0.3, 0.3);
-	c.Insert(0.3, 4.7);
-	c.Insert(4.7, 4.7);
-	c.Insert(4.7, 0.3);
+	c.Insert(0.2, 0.2);
+	c.Insert(0.2, 2.4);
+	c.Insert(4.8, 2.4);
+	c.Insert(4.8, 0.2);
 	sc.AddObject(&c);
 
-	//添加采样边界下面几种都可以
-	SDFSampleBound d(0, WATER, BOUNDSDF_P);
-	d.sdfName = "particle water";
-	d.Insert(2.3, 0.5);
-	d.Insert(2.3, 1.5);
-	d.Insert(3.3, 1.5);
-	d.Insert(3.3, 0.5);
+	//添加物体边界
+	SDFBoundary d(0, Object);
+	d.Insert(4.3, 0.25);
+	d.Insert(4.3, 1.5);
+	d.Insert(4.1, 1.5);
+	d.Insert(4.1, 0.65);
+	d.Insert(2.2, 0.65);
+	d.Insert(1.8, 0.25);
 	sc.AddObject(&d);
-
-	SDFSampleBound e(0, WETSAND, BOUNDSDF_P);
-	e.sdfName = "particle sand";
-	e.Insert(3.5, 0.5);
-	e.Insert(3.5, 1.5);
-	e.Insert(4.3, 1.5);
-	e.Insert(4.3, 0.5);
-	sc.AddObject(&e);
 
 	//模拟器初始化
 	Simulator sim;
 	sim.SimulatorInitial(&sc, MPMMulti, EXPLICIT);
-
+	
 	int i = 0;
-	//渲染器初始化，UI显示
+	//渲染器初始化
 	Render2D render(WINDOW_HEIGHT, WINDOW_WIDTH);
-	UI2D ui;
+	
+	//文件输出，打开文件夹
+	File file;
+	file.CreateFolder();
+	
 	while (1)
 	{
 		sim.SimulatorUpdate();
 		render.CanvasDraw(&sc);
-		ui.UIShow(&render.canvas);
+		file.CanvasOutput(i,&render.canvas);
 		render.CanvasClear();
 		i++;
 		cout << i << endl;
 	}
+	
 }
